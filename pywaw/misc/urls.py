8c300a00267b843231a10f67bd447efbf144f3ac
Konrad Hałas <halas.konrad@gmail.com>
2013-04-04 21:33:26 +0200

    Added basic layout draft, homepage and custom css.

--- pywaw/misc/urls.py
+++ pywaw/misc/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 from misc import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.HomeView.as_view()),
+    url(r'^$', views.HomeView.as_view(), name='home'),
 )
