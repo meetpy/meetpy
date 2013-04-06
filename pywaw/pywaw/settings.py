0a6d0bf60601376bbabc3515fb0510643fc93cb4
Konrad Hałas <halas.konrad@gmail.com>
2013-04-07 00:55:22 +0200

    Enabled admin app and configured local db.

--- pywaw/pywaw/settings.py
+++ pywaw/pywaw/settings.py
@@ -11,13 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '../sqlite.db',
     }
 }
 
@@ -119,10 +114,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
 )
 
 # A sample logging configuration. The only tangible logging
--- pywaw/pywaw/urls.py
+++ pywaw/pywaw/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^', include('misc.urls', namespace='misc')),
+    url(r'^admin/', include(admin.site.urls)),
 )
