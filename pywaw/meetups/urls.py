999635ab02460017fbaca50771de00de5e2a7e6c
Konrad Hałas <halas.konrad@gmail.com>
2013-04-07 21:48:14 +0200

    Added simple RSS & ATOM feeds.

--- pywaw/meetups/urls.py
+++ pywaw/meetups/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = patterns('',
     url(r'^meetups$', views.MeetupsListView.as_view(), name='list'),
     url(r'^meetups/(?P<pk>\d+)$', views.MeetupDetailView.as_view(), name='detail'),
+    url(r'^meetups/atom/$', views.MeetupsAtomFeed(), name='atom_feed'),
+    url(r'^meetups/rss/$', views.MeetupsRssFeed(), name='rss_feed'),
 )
--- pywaw/meetups/views.py
+++ pywaw/meetups/views.py
@@ -1,5 +1,8 @@
 from django.views import generic
-from . import models
+from django.contrib.syndication import views as syndication_views
+from django.core.urlresolvers import reverse_lazy
+from django.utils import feedgenerator
+from . import models, constants
 
 
 class MeetupsListView(generic.ListView):
@@ -8,3 +11,17 @@ class MeetupsListView(generic.ListView):
 
 class MeetupDetailView(generic.DetailView):
     model = models.Meetup
+
+
+class MeetupsRssFeed(syndication_views.Feed):
+    title = constants.FEED_TITLE
+    link = reverse_lazy('meetups:list')
+    title_template = models.Meetup._meta.app_label + '/feed/meetup_title.txt'
+    description_template = models.Meetup._meta.app_label + '/feed/meetup_description.html'
+
+    def items(self):
+        return models.Meetup.objects.all()
+
+
+class MeetupsAtomFeed(MeetupsRssFeed):
+    feed_type = feedgenerator.Atom1Feed
