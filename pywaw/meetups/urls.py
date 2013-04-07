7a0c2d7cbb2f420bde72f75bf5ca8dd148ef99a0
Konrad Hałas <halas.konrad@gmail.com>
2013-04-07 21:52:44 +0200

    Simplify urls.

--- pywaw/meetups/urls.py
+++ pywaw/meetups/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = patterns('',
-    url(r'^meetups$', views.MeetupsListView.as_view(), name='list'),
-    url(r'^meetups/(?P<pk>\d+)$', views.MeetupDetailView.as_view(), name='detail'),
-    url(r'^meetups/atom/$', views.MeetupsAtomFeed(), name='atom_feed'),
-    url(r'^meetups/rss/$', views.MeetupsRssFeed(), name='rss_feed'),
+    url(r'^$', views.MeetupsListView.as_view(), name='list'),
+    url(r'^(?P<pk>\d+)/$', views.MeetupDetailView.as_view(), name='detail'),
+    url(r'^atom/$', views.MeetupsAtomFeed(), name='atom_feed'),
+    url(r'^rss/$', views.MeetupsRssFeed(), name='rss_feed'),
 )
