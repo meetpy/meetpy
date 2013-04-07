9ebe3cdf6641d221554c6c57c6639a9d5cfd89dc
Konrad Hałas <halas.konrad@gmail.com>
2013-04-07 02:04:42 +0200

    Added Speaker and Talk models.

--- pywaw/meetups/admin.py
+++ pywaw/meetups/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Meetup)
+admin.site.register(models.Speaker)
+admin.site.register(models.Talk)
--- pywaw/meetups/models.py
+++ pywaw/meetups/models.py
@@ -6,8 +6,34 @@ class Meetup(models.Model):
     name = models.CharField(max_length=100)
     date = models.DateTimeField()
 
+    class Meta:
+        ordering = ['-date']
+
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('meetups:detail', kwargs={'pk': self.id})
+
+
+class Speaker(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    website = models.URLField(blank=True)
+
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+
+class Talk(models.Model):
+    title = models.CharField(max_length=1000)
+    description = models.TextField(blank=True)
+    speaker = models.ForeignKey(Speaker, related_name='speakers')
+    meetup = models.ForeignKey(Meetup, related_name='talks')
+    time = models.TimeField()
+
+    class Meta:
+        ordering = ['time']
+
+    def __str__(self):
+        return '{} - {}'.format(self.title, self.speaker)
