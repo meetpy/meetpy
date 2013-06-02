eece9cb12e6bbdc69617e1653735aefabb9ecee1
Konrad Hałas <halas.konrad@gmail.com>
2013-06-02 19:13:12 +0200

    Fixed meetups list url.

--- pywaw/meetups/urls.py
+++ pywaw/meetups/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.MeetupsListView.as_view(), name='list'),
+    url(r'^historia/$', views.MeetupsListView.as_view(), name='list'),
     url(r'^(?P<pk>\d+)/$', views.MeetupDetailView.as_view(), name='detail'),
     url(r'^(?P<day>\d{2})-(?P<month>\d{2})-(?P<year>\d{4})/$', views.MeetupDateRedirectView.as_view(),
         name='date_redirect'),
