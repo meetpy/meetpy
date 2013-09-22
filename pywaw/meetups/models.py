8e910e2bf755b56f821497e1958474d2d83924a8
Konrad Hałas <halas.konrad@gmail.com>
2013-09-22 21:56:27 +0200

    Added biography field to speaker.

--- pywaw/meetups/models.py
+++ pywaw/meetups/models.py
@@ -79,6 +79,7 @@ class Speaker(models.Model):
     )
     phone = models.CharField(max_length=30, blank=True)
     email = models.EmailField(blank=True)
+    biography = models.TextField(blank=True)
 
     def __str__(self):
         return '{} {}'.format(self.first_name, self.last_name)
