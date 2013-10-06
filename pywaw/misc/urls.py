eb9207d0acedad8c5804aab509b95dc8d345b9b7
Przemek Lewandowski <haxoza@users.noreply.github.com>
2013-10-06 02:11:56 +0200

    Added sitemap.

--- pywaw/misc/urls.py
+++ pywaw/misc/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, url
 from . import views
+from .sitemaps import sitemaps
 
 urlpatterns = patterns('',
     url(r'^$', views.HomeView.as_view(), name='home'),
+    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
 )
--- pywaw/pywaw/settings/base.py
+++ pywaw/pywaw/settings/base.py
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'django.contrib.sitemaps',
     'south',
     'meetups',
 )
