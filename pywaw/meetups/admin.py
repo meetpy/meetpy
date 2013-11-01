0e6bb18706fe857072a944209b3fa50609ea9a91
Konrad Hałas <halas.konrad@gmail.com>
2013-11-01 23:56:41 +0100

    Merged with default.

---
+++ pywaw/meetups/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from . import models
+
+admin.site.register(models.Meetup)
+admin.site.register(models.Speaker)
+admin.site.register(models.Talk)
+admin.site.register(models.Sponsor)
+admin.site.register(models.Venue)
+admin.site.register(models.ExternalLink)
