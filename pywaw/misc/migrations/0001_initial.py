75c840b180d03781521ec043cebf311026cf4843
Konrad Hałas <halas.konrad@gmail.com>
2014-01-07 21:43:09 +0100

    Merged with default branch.

---
+++ pywaw/misc/migrations/0001_initial.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Partner'
+        db.create_table('misc_partner', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('url', self.gf('django.db.models.fields.URLField')(max_length=200)),
+            ('logo', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
+            ('is_public', self.gf('django.db.models.fields.BooleanField')(default=False)),
+        ))
+        db.send_create_signal('misc', ['Partner'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Partner'
+        db.delete_table('misc_partner')
+
+
+    models = {
+        'misc.partner': {
+            'Meta': {'object_name': 'Partner'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_public': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'logo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'})
+        }
+    }
+
+    complete_apps = ['misc']
