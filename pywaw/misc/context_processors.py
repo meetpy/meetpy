273c8b4141ca7c257f6c81b87d306092d1fcc67f
Konrad Hałas <halas.konrad@gmail.com>
2014-01-11 22:28:47 +0100

    Added website revision hash to stack.

--- pywaw/misc/context_processors.py
+++ pywaw/misc/context_processors.py
@@ -1,5 +1,8 @@
+import functools
+import subprocess
 import django
 import platform
+from django.conf import settings
 from django.contrib.sites.models import get_current_site
 
 
@@ -8,6 +11,7 @@ def system_info(request):
         'system': {
             'django': django.get_version(),
             'python': platform.python_version(),
+            'website': get_website_version(),
         }
     }
 
@@ -16,3 +20,10 @@ def current_site(request):
     return {
         'site': get_current_site(request),
     }
+
+
+@functools.lru_cache(maxsize=1)
+def get_website_version():
+    command = 'hg tip --template {node}'
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, cwd=settings.PROJECT_ROOT)
+    return process.communicate()[0]
