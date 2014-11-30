b10e8b4f9bbac96f7819367e6ea0616a30929f6d
Konrad Hałas <halas.konrad@gmail.com>
2014-11-30 18:08:12 +0100

    Improve admin section.

--- pywaw/meetups/admin.py
+++ pywaw/meetups/admin.py
@@ -20,11 +20,21 @@ class MeetupAdmin(admin.ModelAdmin):
 
 class TalkProposalAdmin(admin.ModelAdmin):
     readonly_fields = ('talk', 'message', 'date_submitted')
+    list_display = ('talk', 'message', 'date_submitted', 'get_meetup')
+
+    def get_meetup(self, obj):
+        return obj.talk.meetup
+
+    get_meetup.short_description = 'Meetup'
+
+
+class TalkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'meetup')
 
 
 admin.site.register(models.Meetup, MeetupAdmin)
 admin.site.register(models.Speaker)
-admin.site.register(models.Talk)
+admin.site.register(models.Talk, TalkAdmin)
 admin.site.register(models.Sponsor)
 admin.site.register(models.Venue)
 admin.site.register(models.TalkProposal, TalkProposalAdmin)
