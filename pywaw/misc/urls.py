249755f4550485ee1854810e96e4f30c9bf1f643
Piotr Betkier <pbetkier@users.noreply.github.com>
2014-11-30 15:32:50 +0100

    Add FAQ page.

--- pywaw/misc/urls.py
+++ pywaw/misc/urls.py
@@ -4,5 +4,6 @@ from .sitemaps import sitemaps
 
 urlpatterns = patterns('',
     url(r'^$', views.HomeView.as_view(), name='home'),
+    url(r'^faq/$', views.FaqView.as_view(), name='faq'),
     url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
 )
