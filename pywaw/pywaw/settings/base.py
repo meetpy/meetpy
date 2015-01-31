8d7499532270b3913a199c4b5b9c3abf007737a4
konradhalas <halas.konrad@gmail.com>
2015-01-31 19:48:47 +0100

    Add server IP to allowed hosts.

--- pywaw/pywaw/settings/base.py
+++ pywaw/pywaw/settings/base.py
@@ -21,7 +21,7 @@ MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['.pywaw.org']
+ALLOWED_HOSTS = ['.pywaw.org', '178.62.28.109']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
