2da05d443070aaa1b99a53efc80cd605f95e2e9e
Artur Czepiel <czepiel.artur@gmail.com>
2018-05-02 09:51:34 +0200

    added provision-pykonik.sh script for new dev environments

---
+++ provision-pykonik.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+function title {
+    echo -e "\n\n\n" $1 "\n--------"
+}
+
+
+title "Add upstream remote"
+git remote add upstream git@github.com:pykonik/pykonik.org.git
+
+title "Create virtualenv"
+virtualenv .pykonik-env
+source ./.pykonik-env/bin/activate
+
+title "Run pip install"
+# currently there are no dev requirements file,
+# but when there is – we should add it here.
+pip install -r requirements/base.txt
+
+cd pykonik/
+
+title "Setup basic secrets"
+mkdir pykonik/settings/pykonik_secret_variables
+
+
+cat <<EOF > pykonik/settings/pykonik_secret_variables/base.json
+{
+    "ADMIN_EMAIL": "foo@example.com",
+    "SECRET_KEY": "asdf",
+    "ALLOWED_HOSTS": ["*"]
+}
+EOF
+
+title "Run migrations"
+./manage.py migrate
+
+title "Run tests"
+./manage.py test
+
+title "Create superuser"
+./manage.py createsuperuser
+
+title "Runserver"
+export DJANGO_SETTINGS_MODULE="pykonik.settings.local"
+./manage.py runserver
