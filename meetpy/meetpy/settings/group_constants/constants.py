f1a83806e38559c5786345c35d526a5066278cc8
procopio matteo <procopio.matteo1@gmail.com>
2019-10-05 16:11:50 +0200

    easier new groups setup

--- meetpy/meetpy/settings/group_constants/constants.py
+++ meetpy/meetpy/settings/group_constants/constants.py
@@ -3,15 +3,15 @@ import environ
 
 env = environ.Env()
 
-GROUP_NAME = env('GROUP_NAME')  # uppercase
-GROUP_PAGE_ADDRESS_SHORT = env('GROUP_PAGE_ADDRESS_SHORT')
-GROUP_PAGE_ADDRESS_LONG = env('GROUP_PAGE_ADDRESS_LONG')
-GROUP_CITY = env('GROUP_CITY')
-GROUP_CITY_GENITIVE = env('GROUP_CITY_GENITIVE') # examples: Krakowa, Warszawy
-GROUP_CITY_ABLATIVE = env('GROUP_CITY_ABLATIVE')  # examples: Krakowie, Warszawie
-GROUP_CITY_ADJECTIVE = env('GROUP_CITY_ADJECTIVE')  # examples: Krakowska, Warszawska
-GROUP_CITY_LOCATIVE = env('GROUP_CITY_LOCATIVE ') # examples: krakowskiej, warszawskiej
-CONTACT_EMAIL = env('CONTACT_EMAI')
+GROUP_NAME = env('GROUP_NAME', default='GROUP_NAME')  # uppercase
+GROUP_PAGE_ADDRESS_SHORT = env('GROUP_PAGE_ADDRESS_SHORT', default='GROUP_PAGE_ADDRESS_SHORT')
+GROUP_PAGE_ADDRESS_LONG = env('GROUP_PAGE_ADDRESS_LONG', default='GROUP_PAGE_ADDRESS_LONG')
+GROUP_CITY = env('GROUP_CITY', default='GROUP_CITY')
+GROUP_CITY_GENITIVE = env('GROUP_CITY_GENITIVE', default='GROUP_CITY_GENITIVE') # examples: Krakowa, Warszawy
+GROUP_CITY_ABLATIVE = env('GROUP_CITY_ABLATIVE', default='GROUP_CITY_ABLATIVE')  # examples: Krakowie, Warszawie
+GROUP_CITY_ADJECTIVE = env('GROUP_CITY_ADJECTIVE', default='GROUP_CITY_ADJECTIVE')  # examples: Krakowska, Warszawska
+GROUP_CITY_LOCATIVE = env('GROUP_CITY_LOCATIVE ', default='GROUP_CITY_LOCATIVE') # examples: krakowskiej, warszawskiej
+CONTACT_EMAIL = env('CONTACT_EMAIL', default='CONTACT_EMAIL')
 LOGO_PATH = 'group/group_logo.png'
 GITHUB = 'https://github.com/meetpy/meetpy'
 
@@ -23,6 +23,6 @@ SOCIAL_MEDIA = {
     'youtube': 'http://youtube.com/pykonik',
 }
 
-FEED_TITLE = env('FEED_TITLE')
+FEED_TITLE = env('FEED_TITLE', default='FEED_TITLE')
 EITHER_EXISTING_OR_NEW_SPEAKER_ERROR = 'Wybierz prelegenta z listy lub wprowadź dane dla nowego.'
 PRESENTATION_LENGTH = 25
