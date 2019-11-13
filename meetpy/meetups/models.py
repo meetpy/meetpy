d004768c5dff2d7d7998f3a55a51cc65b98adccd
Jakub Wasielak <kuba.wasielak@gmail.com>
2019-11-13 22:05:38 +0100

    Specific templates for a given meetup (#56)

--- meetpy/meetups/models.py
+++ meetpy/meetups/models.py
@@ -77,7 +77,7 @@ class Meetup(models.Model):
         ordering = ['-date']
 
     def __str__(self):
-        name = settings.GROUP_NAME
+        name = settings.CONSTANT['GROUP_NAME']
         if self.meetup_type:
             name = self.meetup_type.name
         return '{0} #{1}'.format(name, self.number)
